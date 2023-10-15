336581bd7913bc72444b7ec8eac022a5f46c6fc6
Geoffrey Martin <geoffm39@gmail.com>
2023-10-15 11:48:56 +1100

    set toplevel mainframes and stacking order

--- gui/windows/edit_logo_window.py
+++ gui/windows/edit_logo_window.py
@@ -5,3 +5,9 @@ class EditLogoWindow(Toplevel):
     def __init__(self, parent):
         super().__init__(parent)
         self.title("Logo Editor")
+        self.attributes('-topmost', 1)
+
+        mainframe = ttk.Frame(self)
+        mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure(0, weight=1)
