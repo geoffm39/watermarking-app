f5c038c6a4492d61f172cdce7ca6f06126906353
Geoffrey Martin <geoffm39@gmail.com>
2023-10-17 21:57:25 +1100

    add select_image to editing canvas

--- editing_canvas.py
+++ editing_canvas.py
@@ -12,3 +12,20 @@ class EditingCanvas(Canvas):
 
         self.images = images
         self.thumbnails = thumbnails
+        self.current_image = None
+        # SHOULD I CREATE ANOTHER LIST WITH EDITING IMAGES SETTING A LARGE THUMBNAIL TO KEEP SAME SIZE??
+        # if i do this how can i set the wanted position of the text and logo with dragging around???
+        # need to apply the text and logos to a percentage location and size relevent to thumbnail for each image
+
+        # THE SCROLLING WILL MOVE THESE! disable these buttons after picking first image to edit
+        # if these move around the screen as changing images, just remove them! (add buttons up top instead)
+        self.next_button_img = ImageTk.PhotoImage('../images/next_arrow.png')
+        self.back_button_img = ImageTk.PhotoImage('../images/back_arrow.png')
+        self.next_arrow_button = ttk.Button(self.editing_frame, image=self.next_button_img)
+        self.back_arrow_button = ttk.Button(self.editing_frame, image=self.back_button_img)
+
+    def select_image(self, image_index):
+        self.current_image = self.images[image_index]
+        tk_version = ImageTk.PhotoImage(self.current_image)
+        image_label = ttk.Label(self.editing_frame, image=tk_version)
+        image_label.grid()
